78d887193344638499d237f434c05e24eab1e592
Christopher Reece <mrkita.reece@gmail.com>
2018-03-12 21:10:09 +0900

    added formate string

---
+++ python/formate-string.py
@@ -0,0 +1,5 @@
+def print_formatted(number):
+    w = len(bin(number)[2:])
+
+    for i in range(1, n + 1):
+        print (str(i).rjust(w,' '),str(oct(i)[2:]).rjust(w,' '),str(hex(i)[2:].upper()).rjust(w,' '),str(bin(i)[2:]).rjust(w,' '),sep=' ')
