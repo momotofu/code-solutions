afc3c3805218648177b29912ff840d25c9065b03
Christopher Reece <mrkita.reece@gmail.com>
2018-05-01 11:41:59 +0900

    added demo_2.py

--- codility/demo.py
+++ codility/demo.py
@@ -1,8 +1,9 @@
 # given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
 
-# single item
-# all negative
-# all positive
+# extreme single element
+# extreme minimal and maximal values
+# shuffled sequence
+# negative only
 
 def solution(A):
     # single item
---
+++ codility/demo_2.py
@@ -0,0 +1,26 @@
+
+def solution(A):
+    # single element
+    if len(A) == 1:
+        cur = A[0]
+        return 2 if cur == 1 else 1
+
+    # sort array and remove duplicates
+    a = list(set(sorted(A)))
+
+    # all negative
+    if a[-1] < 1:
+        return 1
+
+    # all other cases
+    min_num = 1 # keep track of minum possible number
+
+    # iterate through each item and compare / update min_num
+    for i in range(len(a)):
+        cur = a[i]
+        if cur == min_num:
+            min_num += 1
+        elif cur > min_num:
+            return min_num
+
+    return min_num
