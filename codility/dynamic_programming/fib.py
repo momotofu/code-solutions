e8cc316ed2a6925898e2d04f1ea1bdb5a9df38e4
Christopher Reece <mrkita.reece@gmail.com>
2018-05-02 19:18:20 +0900

    added bottom up fib

--- codility/dynamic_programming/fib.py
+++ codility/dynamic_programming/fib.py
@@ -1,3 +1,4 @@
+# top-down approach
 def fib(n, lookup):
     """
     Fib using a lookup / momoization
@@ -12,3 +13,17 @@ def fib(n, lookup):
 
     # Return the value corresponding to n
     return lookup[n]
+
+# bottom-up approach
+def fib_bu(n):
+    # array declaration
+    f = [0]*(n + 1)
+
+    # base case assignment
+    f[1] = 1
+
+    # calculating the fibonacci and storing the values
+    for i in range(2, n + 1):
+        f[i] = f[i - 1] + f[i - 2]
+
+    return f
